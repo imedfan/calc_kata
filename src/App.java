d573b847e9a9edd0371c3b483ded00164e49619e
imedfan <medfan92@gmail.com>
2022-04-18 10:46:34 +0200

    add Exception

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
     static int number1, number2, result; // вынес из майна, чтоб на всех
     static char symbol;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CalcException {
         Scanner console = new Scanner(System.in);
         String inputConsole = console.nextLine(); // снимем ввод для начала
         char[] inputConsoleMass = inputConsole.toCharArray();
@@ -17,11 +17,17 @@ public class App {
             if(c=='/' || c=='*' || c=='+' || c=='-'){
                 ii+=1;
                 if(ii>=2){
-                    System.out.println("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-                    System.exit(1);
+                    //System.out.println("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+                    throw new CalcException("Формат математической операции не удовлетворяет заданию");
+                    //System.exit(1);
                 }
             }
         }
+
+        if(ii<=0){
+            throw new CalcException("Строка не является математической операцией");
+        }
+
         String[] inputConsoleMassStrMass = inputConsoleMassStr.split("[ ]"); // на питоне с таким проще
         number1 = RomatoAraba(inputConsoleMassStrMass[0]);
         number2 = RomatoAraba(inputConsoleMassStrMass[2]);
@@ -33,7 +39,8 @@ public class App {
             result=calca(number1, number2, symbol);
             System.out.println(result); //на арабской мове
         }else if(number1<0 || number2<0){ // проверка на римскоарабскую 
-            System.out.println("Используются одновременно разные системы счисления");
+            //System.out.println("Используются одновременно разные системы счисления");
+            throw new CalcException("Используются одновременно разные системы счисления");
         }else{ // если арабские
                 symbol = inputConsoleMassStrMass[1].charAt(0);
                 result=calca(number1, number2, symbol);
@@ -41,7 +48,8 @@ public class App {
                     String  resultRoman = convertNumToRoman(result);
                     System.out.println(resultRoman); //на римской мове
                 }else if(result<0){
-                    System.out.println("В римской системе нет отрицательных чисел");
+                    //System.out.println("В римской системе нет отрицательных чисел");
+                    throw new CalcException("В римской системе нет отрицательных чисел");
                 }
                 
             }
@@ -50,7 +58,7 @@ public class App {
         
     
 
-    public static int calca(int n1, int n2, char sym) { // калькуляция
+    public static int calca(int n1, int n2, char sym) throws CalcException { // калькуляция
         int res = 0;
         if (n1 <= 10 && n2 <= 10) {
             switch (sym) {
@@ -61,8 +69,9 @@ public class App {
                    // break;
                 case '/':
                     if (n2 == 0) {
-                        System.out.println("На ноль делить нельзя!");
-                        break;
+                        //System.out.println("На ноль делить нельзя!");
+                        throw new CalcException("На ноль делить нельзя!");
+                        
                     } else {
                         res = n1 / n2;
                         return res;
@@ -81,16 +90,17 @@ public class App {
                     //System.out.println(res);
                     //break;
                 default:
-                System.out.println("Отсутствует оператор");
-                System.exit(1);
+                //System.out.println("Отсутствует оператор");
+                throw new CalcException("Отсутствует оператор");
                     
             }
         } else {
-            System.out.println("Калькулятор принимает на вход числа от 1 до 10 включительно, не более.");
-            System.exit(1);
+            //System.out.println("Калькулятор принимает на вход числа от 1 до 10 включительно, не более.");
+            //System.exit(1);
+            throw new CalcException("Калькулятор принимает на вход числа от 1 до 10 включительно, не более.");
         }
 
-        return res;
+        //return res;
     }
 
     public static int RomatoAraba(String roman) { //спасибо кто все это писал ручками 
@@ -145,3 +155,5 @@ public class App {
     }
 
 }
+
+
